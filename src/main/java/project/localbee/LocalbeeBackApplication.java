ad1a5c62450f6535bbeeebc5a92433fa7e6ca0bc
jangwanjung <tjdrj530@naver.com>
2024-11-14 10:53:23 +0900

    프로젝트 생성

---
+++ src/main/java/project/localbee/LocalbeeBackApplication.java
@@ -0,0 +1,13 @@
+package project.localbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalbeeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalbeeBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/localbee/LocalbeeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package project.localbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalbeeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
