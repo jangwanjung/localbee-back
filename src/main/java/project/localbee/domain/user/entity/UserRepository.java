354fe3fd5b4578baa3c407d8a4dd493f872690f1
jangwanjung <tjdrj530@naver.com>
2024-11-18 14:17:17 +0900

    줄바꿈

--- src/main/java/project/localbee/domain/user/entity/UserRepository.java
+++ src/main/java/project/localbee/domain/user/entity/UserRepository.java
@@ -1,5 +1,6 @@
 package project.localbee.domain.user.entity;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
