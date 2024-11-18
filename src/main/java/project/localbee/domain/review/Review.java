d9f22d676fe0889b9fd023345df53816781d88dc
jangwanjung <tjdrj530@naver.com>
2024-11-18 12:07:17 +0900

    review 테이블추가

---
+++ src/main/java/project/localbee/domain/review/Review.java
@@ -0,0 +1,47 @@
+package project.localbee.domain.review;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import project.localbee.domain.Like.Like;
+import project.localbee.domain.travel.Travel;
+import project.localbee.domain.user.User;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "travel_id")
+    private Travel travel;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "review")
+    @JsonIgnoreProperties({"review"})
+    private List<Like> likes;
+
+    @CreationTimestamp
+    private Timestamp created_at;
+
+    @Transient
+    private int like_count;
+}
---
+++ src/main/java/project/localbee/domain/review/ReviewRepository.java
@@ -0,0 +1,6 @@
+package project.localbee.domain.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
