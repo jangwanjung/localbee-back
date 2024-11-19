b2a39e37e6a041a99b4ef66e9f5d31aff8b97920
jangwanjung <tjdrj530@naver.com>
2024-11-19 17:15:45 +0900

    RoleType에 value값추가

--- src/main/java/project/localbee/domain/user/entity/RoleType.java
+++ src/main/java/project/localbee/domain/user/entity/RoleType.java
@@ -1,6 +1,15 @@
 package project.localbee.domain.user.entity;
 
+import lombok.Getter;
 
+
+@Getter
 public enum RoleType {
-    USER,ADMIN,GUIDE
+    USER("ROLE_USER"),ADMIN("ROLE_ADMIN"),GUIDE("ROLE_GUIDE");
+
+    private String key;
+
+    RoleType(String key) {
+        this.key = key;
+    }
 }
